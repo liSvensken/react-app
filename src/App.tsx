05f6157e75f4119cd8913e0af24b887d4d441011
elizaveta.shvedchikova <eli3avetta.shvedchikova@gmail.com>
2023-04-08 17:11:29 +0300

    переключатель светлой-темной темы

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useContext, useState } from 'react';
 import { Link, Route, Routes } from 'react-router-dom';
-import './index.scss';
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import './styles/index.scss';
+import { Theme, ThemeContext } from "./theme/ThemeContext";
+import { UseTheme } from "./theme/useTheme";
 
 const App = () => {
+	const { theme, toggleTheme } = UseTheme();
+
 	return (
-		<div className="app">
+		<div className={ `app ${ theme }` }>
+			<button onClick={ toggleTheme }>Toggle theme</button>
+			<br/>
 			<Link to={ '/' }>Main</Link>
+			<br/>
 			<Link to={ '/about' }>About site</Link>
 			<Suspense fallback={ <div>Loading...</div> }>
 				<Routes>
