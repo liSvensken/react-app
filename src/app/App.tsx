812c837fb6621274b460043a51c35d7b20c32b8e
eli3avettaSh <e.shvedchikova@zakupki360.ru>
2023-04-16 12:20:36 +0300

    AppRouter. Конфиг для роутера

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,9 @@
-import React, { Suspense } from 'react';
-import { Link, Route, Routes } from 'react-router-dom';
+import React from 'react';
+import { Link } from 'react-router-dom';
 import './styles/index.scss';
 import { UseTheme } from "app/providers/ThemeProvider";
 import { classNames } from "shared/lib/helpers/classNames";
-import { AboutPage } from "pages/AboutPage";
-import { MainPage } from "pages/MainPage";
+import { AppRouter } from "app/providers/router";
 
 const App = () => {
 	const { theme, toggleTheme } = UseTheme();
@@ -16,12 +15,8 @@ const App = () => {
 			<Link to={ '/' }>Main</Link>
 			<br/>
 			<Link to={ '/about' }>About site</Link>
-			<Suspense fallback={ <div>Loading...</div> }>
-				<Routes>
-					<Route path={ '/about' } element={ <AboutPage/> }/>
-					<Route path={ '/' } element={ <MainPage/> }/>
-				</Routes>
-			</Suspense>
+
+			<AppRouter/>
 		</div>
 	);
 };
