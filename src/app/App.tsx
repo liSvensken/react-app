39af8304b7ebbe3e7433a0a0c29f147d47b49ad7
Elizaveta.Shvedchikova <Elizaveta.Shvedchiko@tatar.ru>
2023-04-26 17:02:40 +0300

    changes loader

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,13 +5,14 @@ import { classNames } from 'shared/lib/helpers/classNames';
 import { AppRouter } from 'app/providers/router';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
+import { PageLoader } from 'widgets/PageLoader';
 
 const App = () => {
   const { theme } = UseTheme();
 
   return (
     <div className={ classNames('app', {}, [theme]) }>
-      <Suspense fallback="loading">
+      <Suspense fallback={ <PageLoader /> }>
         <Navbar />
         <div className="content-page">
           <Sidebar />
--- src/shared/ui/LoaderSpinner/ui/LoaderSpinner.tsx
+++ src/shared/ui/LoaderSpinner/ui/LoaderSpinner.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, useMemo } from 'react';
 import { classNames } from 'shared/lib/helpers/classNames';
 import cls from './LoaderSpinner.module.scss';
 
@@ -21,47 +21,37 @@ export const LoaderSpinner: FC<LoaderSpinnerProps> = (
 ) => {
   const loaderItems = new Array(12).fill(null);
 
-  // todo куда-то это надо вынести
-  const getStylesWrapper = () => {
-    if (size) {
-      return {
-        height: `${size}px`,
-        width: `${size}px`,
-      };
-    }
-    return null;
-  };
+  const getStylesWrapper = useMemo(() => ({
+    height: `${size}px`,
+    width: `${size}px`,
+  }), [size]);
 
-  const getStylesItem = () => {
-    return {
-      transformOrigin: `${size / 2}px ${size / 2}px`,
-    };
-  };
+  const getStylesItem = useMemo(() => ({
+    transformOrigin: `${size / 2}px ${size / 2}px`,
+  }), [size]);
 
-  const getStylesItemChild = () => {
-    return {
-      left: `${size / 2.16}px`,
-      width: `${widthLine}px`,
-      height: `${heightLine}px`,
-      borderRadius: `${borderLinePercent}%`,
-    };
-  };
+  const getStylesItemChild = useMemo(() => ({
+    left: `${size / 2.16}px`,
+    width: `${widthLine}px`,
+    height: `${heightLine}px`,
+    borderRadius: `${borderLinePercent}%`,
+  }), [size, widthLine, heightLine, borderLinePercent]);
 
   return (
     <div
       className={ classNames(cls.loaderSpinner, {}, [className]) }
-      style={ getStylesWrapper() }
+      style={ getStylesWrapper }
     >
       {
         loaderItems.map((index) => (
           <div
             key={ index }
             className={ classNames(cls.loaderItem) }
-            style={ getStylesItem() }
+            style={ getStylesItem }
           >
             <div
               className={ classNames(cls.loaderItemChild) }
-              style={ getStylesItemChild() }
+              style={ getStylesItemChild }
             />
           </div>
         ))
